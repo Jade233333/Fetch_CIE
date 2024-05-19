673f46635322fd8ea96621c0ba6ec1c63cee8eb4
Kathy <hatesjade@gmail.com>
2024-05-19 19:20:33 +0800

    if __main__ protection

--- fetch_CIE.py
+++ fetch_CIE.py
@@ -27,34 +27,34 @@ def get_input(prompt, items, default=None):
     elif default is not None:
         items.append(default)
 
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--destination", help="where to store downloaded file", default="downloads")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--destination", help="where to store downloaded file", default="downloads")
+    args = parser.parse_args()
 
 # Initialize empty lists for each item
-codes = []
-seasons = []
-years = []
-paper_types = []
-component_numbers = []
-time_zones = []
+    codes = []
+    seasons = []
+    years = []
+    paper_types = []
+    component_numbers = []
+    time_zones = []
 
 # Prompt user for input for each item
-get_input("Enter code(s) (default 9701): ", codes, "9701")
-get_input("Enter season(s) (default s): ", seasons, "s")
-get_input("Enter year(s) (default 23): ", years, "23")
-get_input("Enter paper type(s) (default qp): ", paper_types, "qp")
-get_input("Enter component number(s) (default 2): ",component_numbers, "2")
-get_input("Enter time zone(s) (default 1): ", time_zones, "1")
+    get_input("Enter code(s) (default 9701): ", codes, "9701")
+    get_input("Enter season(s) (default s): ", seasons, "s")
+    get_input("Enter year(s) (default 23): ", years, "23")
+    get_input("Enter paper type(s) (default qp): ", paper_types, "qp")
+    get_input("Enter component number(s) (default 2): ",component_numbers, "2")
+    get_input("Enter time zone(s) (default 1): ", time_zones, "1")
 
 # URL template
-url_template = "https://cie.fraft.cn/obj/Fetch/redir/{}_{}{}_{}_{}{}.pdf"
+    url_template = "https://cie.fraft.cn/obj/Fetch/redir/{}_{}{}_{}_{}{}.pdf"
 
 # Generate URLs using itertools.product
-urls = [url_template.format(*combo) for combo in itertools.product(codes, seasons, years, paper_types, component_numbers, time_zones)]
-print(urls)
+    urls = [url_template.format(*combo) for combo in itertools.product(codes, seasons, years, paper_types, component_numbers, time_zones)]
+    print(urls)
 
 # Use multi-treading to download the files
-with ThreadPoolExecutor() as executor:
-    executor.map(download_file, urls)
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_file, urls)
